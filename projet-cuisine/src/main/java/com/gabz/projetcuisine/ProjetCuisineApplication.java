64d867f8d3eb613f5702dcc3e1865b465b7c7e8e
Gabz98 <gabinritz@gmail.com>
2018-12-03 15:48:19 +0100

    initialized java project

---
+++ projet-cuisine/src/main/java/com/gabz/projetcuisine/ProjetCuisineApplication.java
@@ -0,0 +1,18 @@
+package com.gabz.projetcuisine;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCuisineApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetCuisineApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World");
+    }
+}
