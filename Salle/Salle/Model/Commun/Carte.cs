a7fb5857f552085f5dea2bb9f8afe7434c8cd749
pirmin6 <pirmin.meyerts4@gmail.com>
2018-12-06 12:01:41 +0100

    Classe Commun

---
+++ Salle/Salle/Model/Commun/Carte.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salle.Model.Commun
+{
+    class Carte
+    {
+        List<Plat> plats = new List<Plat>();
+        List<Vin> vins = new List<Vin>();
+        int Date; // (string timedate.tostring)((((((
+
+        public Carte(List<Plat> plats, List<Vin> vins)
+        {
+
+     
+        }
+    }
+
+    
+}
