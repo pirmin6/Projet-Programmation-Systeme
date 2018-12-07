50d8f519a8e09278c58853742a447d7ecbbf40d0
pirmin6 <pirmin.meyerts4@gmail.com>
2018-12-07 11:29:16 +0100

    Classes controler + view

---
+++ Salle/Salle/View/AbstractView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salle.View
+{
+    abstract class AbstractView : EmployeView, ClientView, MaterielView, PlatView
+    {
+        void initialiserComposant()
+        {
+
+        }
+
+        void update()
+        {
+
+        }
+    }
+}
